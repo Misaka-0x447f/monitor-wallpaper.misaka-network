302fd571a381eac5b6d0fa9097225b378bdb8589
Misaka <447f.misaka@outlook.com>
2018-06-10 10:47:37 +0800

    fresh start

--- src/util/index.js
+++ src/util/index.js
@@ -1,11 +1,47 @@
 export default {
-    escapeHTML: function (str) {
-        return str
-            .replace(/&/g, '&amp;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;')
-            .replace(/"/g, '&quot;')
-            .replace(/'/g, '&#39;')
-            .replace(/\//g, '&#x2F;');
+    math: {
+        randInt: function (from, to) {
+            /**
+             * @param from - include
+             * @param to - include
+             */
+            return Math.floor(Math.random() * (to + 1 - from)) + from;
+        }
+    },
+    string: {
+        escapeHTML: function (str) {
+            return str
+                .replace(/&/g, '&amp;')
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;')
+                .replace(/"/g, '&quot;')
+                .replace(/'/g, '&#39;')
+                .replace(/\//g, '&#x2F;');
+        },
+        padding: function (str, format) {
+            format = format.split('.');
+            if (format.length === 3) {
+                return str.padStart(format[0].length).padEnd(format[2].length);
+            } else {
+                return str.padStart(format[0].length);
+            }
+        }
+    },
+    array: {
+        randomIn: function (arr) {
+            return arr[Math.floor(Math.random() * arr.length)];
+        }
+    },
+    timing: {
+        getUnixTimeStamp: function (offset = 0) {
+            return (new Date()).getTime() / 1000 - offset;
+        },
+        awaitTimeout: async function (delay) {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve();
+                }, delay);
+            });
+        }
     }
 };
